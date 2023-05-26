208fe0a2b57a1dd40a79dad31013c2ec8827e00d
YatechDev <106195439+YatechDev@users.noreply.github.com>
2023-05-26 07:18:13 +0100

    Add provisional layout of game menu.

--- run.py
+++ run.py
@@ -1,3 +1,55 @@
 import ascii_art
 
 print(ascii_art.title_art)
+
+
+def print_menu():
+    print("************ PIZZA HERO: THE MUSHROOM QUEST ************")
+    print("-------------------------------------------------------")
+    print("                 1. Start Game")
+    print("                 2. Rules")
+    print("                 3. Exit")
+    print("-------------------------------------------------------")
+
+
+def start_game():
+    """
+    Function to start the game
+    """
+    print("Starting the game...")
+
+
+
+def show_rules():
+    """
+       Function to show the game rules
+       """
+    print("Pizza Hero: The Mushroom Quest")
+    print("------------------------------")
+    print("Game Instructions:")
+    print("This is a text adventure game where you will play as the hero/heroine Bob/Bobina.")
+    print("Your task is to prepare a magical pizza using your great-grandfather's recipe.")
+    print("Throughout the game, you will follow the story, make decisions, and solve puzzles.")
+    print("To make choices, enter the corresponding option numbers.")
+    print("Good luck!")
+    print()
+
+
+def main():
+    print_menu()
+
+    choice = input("Please type the number:\n ")
+
+    if choice == "1":
+        start_game()
+    elif choice == "2":
+        show_rules()
+    elif choice == "3":
+        print("Exiting the game...")
+        return
+    else:
+        print("Wrong command. Please try again\n")
+
+
+if __name__ == "__main__":
+    main()
