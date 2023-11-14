3075924f757b15af0c67d11881b1b776472eed8d
YatechDev <106195439+YatechDev@users.noreply.github.com>
2023-11-14 20:01:37 +0000

    Add 1 Quest to the game

--- game.py
+++ game.py
@@ -3,14 +3,17 @@
 import ascii_art
 import os
 
+
 class Player:
     def __init__(self, name):
         self.name = name
         self.inventory = []
 
+
 def clear_screen():
     # Function to clear the terminal screen
     os.system('clear' if os.name == 'posix' else 'cls')
+
 
 def show_rules():
     clear_screen()
@@ -23,6 +26,7 @@ def show_rules():
     print("To make choices, enter the corresponding option numbers.")
     print("Good luck!\n")
     input("Press Enter to start your adventure...")
+
 
 def start_game(player):
     clear_screen()
@@ -70,10 +74,63 @@ def start_game(player):
 
         # Check the player's answer
         if options[choice - 1] == "An echo":
+            print("\nFunghi: Amazing! You're truly a hero. Let me give you a hint.")
+            input("Press Enter to continue...")
+
             print(
-                "\nFunghi: Correct! You're truly a hero. Take this map; it will guide you to the ingredients for the magical pizza.")
-            player.add_to_inventory("Map")
+                "\nFunghi: Now, you should head to Dark Timmy's place on the edge of the town. He'll guide you further.\n")
+            input("Press Enter to continue...")
+
+            # Start the first quest
+            quest_1(player)
             break
         else:
             print("\nFunghi: That's not the correct answer. Think again!\n")
             input("Press Enter to try the riddle again...")
+
+
+def quest_1(player):
+    clear_screen()
+    print("You embark on a journey to Dark Timmy's place on the edge of the town.\n")
+    input("Press Enter to continue...")
+
+    print("\nAfter a dark and mysterious path, you reach Dark Timmy's residence.\n")
+    input("Press Enter to meet Dark Timmy...")
+
+    print("\nDark Timmy: Greetings, brave adventurer! Welcome to the gloomy side of Mushroomville.")
+    input("Press Enter to continue...")
+
+    print(
+        "\nDark Timmy: I heard you're on a quest for the magical pizza. Before we proceed, would you like to hear a joke?\n")
+    print("1. Yes, tell me the joke.")
+    print("2. No, I'm not in the mood.")
+
+    # Player's choice
+    while True:
+        try:
+            choice = int(input("\nYour choice (enter the number): "))
+            if choice in [1, 2]:
+                break
+            else:
+                print("Invalid choice. Please enter 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    if choice == 1:
+        print("\nDark Timmy: Alright, here's a pizza joke for you:")
+        print("Why did the pizza maker go to jail?\n")
+        input("Press Enter to hear the punchline...")
+
+        print(
+            "\nDark Timmy: Because he couldn't make a good crust! Haha! Well, I find it amusing. Now, back to business.\n")
+        input("Press Enter to continue...")
+        print(
+            "\nDark Timmy: The recipe you seek is indeed a valuable one. However, you can find it in Beatrix's shop at the central market.\n")
+    else:
+        print("\nDark Timmy: No worries. Let's get back to business.")
+        input("Press Enter to continue...")
+        print(
+            "\nDark Timmy: The recipe you seek is indeed a valuable one. However, you can find it in Beatrix's shop at the central market.\n")
+
+    # End of the quest
+    input("Press Enter to continue your adventure...")
