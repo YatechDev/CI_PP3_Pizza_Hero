84b496dd14821041d890965bb4dc00df06244924
YatechDev <106195439+YatechDev@users.noreply.github.com>
2023-11-14 21:35:04 +0000

    Add last quest to the game

--- game.py
+++ game.py
@@ -3,17 +3,13 @@
 import ascii_art
 import os
 
-
 class Player:
     def __init__(self, name):
         self.name = name
         self.inventory = []
 
-
 def clear_screen():
-    # Function to clear the terminal screen
     os.system('clear' if os.name == 'posix' else 'cls')
-
 
 def show_rules():
     clear_screen()
@@ -26,7 +22,6 @@ def show_rules():
     print("To make choices, enter the corresponding option numbers.")
     print("Good luck!\n")
     input("Press Enter to start your adventure...")
-
 
 def start_game(player):
     clear_screen()
@@ -46,21 +41,17 @@ def start_game(player):
           "but first, you must prove yourself.\n")
     input("Press Enter to continue...")
 
-    # Loop for the riddle question
     while True:
         clear_screen()
         print("\nFunghi: To prove your bravery, you need to solve a riddle. Here it is:")
         print(
             "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?\n")
 
-        # Options for the player to choose
         options = ["An echo", "A tree", "A river"]
 
-        # Display options
         for i, option in enumerate(options, start=1):
             print(f"{i}. {option}")
 
-        # Player's choice
         while True:
             try:
                 choice = int(input("\nYour choice (enter the number): "))
@@ -71,7 +62,6 @@ def start_game(player):
             except ValueError:
                 print("Invalid input. Please enter a number.")
 
-        # Check the player's answer
         if options[choice - 1] == "An echo":
             print("\nFunghi: Amazing! You're truly a hero. Let me give you a hint.")
             input("Press Enter to continue...")
@@ -80,13 +70,11 @@ def start_game(player):
                 "\nFunghi: Now, you should head to Dark Timmy's place on the edge of the town. He'll guide you further.\n")
             input("Press Enter to continue...")
 
-            # Start the first quest
             quest_1(player)
             break
         else:
             print("\nFunghi: That's not the correct answer. Think again!\n")
             input("Press Enter to try the riddle again...")
-
 
 def quest_1(player):
     clear_screen()
@@ -104,7 +92,6 @@ def quest_1(player):
     print("1. Yes, tell me the joke.")
     print("2. No, I'm not in the mood.")
 
-    # Player's choice
     while True:
         try:
             choice = int(input("\nYour choice (enter the number): "))
@@ -131,18 +118,16 @@ def quest_1(player):
         print(
             "\nDark Timmy: The recipe you seek is indeed a valuable one. However, you can find it in Beatrix's shop at the central market.\n")
 
-    # End of the quest
     input("Press Enter to continue your adventure...")
 
-    # Start the encounter with a strange character
     encounter_strange_character(player)
 
 def encounter_strange_character(player):
     clear_screen()
-    print("As you make your way to the central market, you encounter a strange character.")
+    print("\nAs you make your way to the central market, you encounter a strange character.")
     input("Press Enter to initiate a conversation...")
 
-    print("\n: Greetings, adventurer! I am Zephyr, the wanderer of Mushroomville.")
+    print("\nZephyr: Greetings, adventurer! I am Zephyr, the wanderer of Mushroomville.")
     input("Press Enter to continue...")
 
     print("\nZephyr: I sense you're on a quest. Would you like some guidance?")
@@ -150,7 +135,6 @@ def encounter_strange_character(player):
     print("2. Escape!")
     print("3. Negotiate.")
 
-    # Player's choice
     while True:
         try:
             choice = int(input("\nYour choice (enter the number): "))
@@ -176,5 +160,49 @@ def encounter_strange_character(player):
     print("\nYou successfully navigate through the encounter and arrive at the central market.")
     input("Press Enter to meet Beatrix and start the next quest...")
 
-    # # Start the third quest
-    # quest3(player)
+    quest_3(player)
+
+def quest_3(player):
+    clear_screen()
+    print("You reach the central market and find Beatrix's magical pizza shop.\n")
+    input("Press Enter to meet Beatrix...")
+
+    print("\nBeatrix: Ah, you must be the brave adventurer seeking the magical pizza recipe. Welcome!\n")
+    input("Press Enter to continue...")
+
+    print("Beatrix hands you a slice of her famous magical pizza.")
+    input("Press Enter to make your choice...")
+
+    print("\n1. Eat the pizza.")
+    print("2. Refuse the pizza.")
+
+    while True:
+        try:
+            choice = int(input("\nYour choice (enter the number): "))
+            if choice in [1, 2]:
+                break
+            else:
+                print("Invalid choice. Please enter 1 or 2.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+    if choice == 1:
+        print("\nBeatrix: A wise choice! Enjoy the magical pizza.\n")
+        input("Press Enter to savor the magical taste...")
+
+        game_completed(player)
+    else:
+        print("\nBeatrix: A peculiar decision, but I respect it. The quest continues...\n")
+        input("Press Enter to proceed...")
+
+# Continue the story with a new quest
+
+
+def game_completed(player):
+    clear_screen()
+    print("Congratulations, " + player.name + "!")
+    print("You have successfully completed the Pizza Hero: The Mushroom Quest.\n")
+    print("Beatrix: You have proven yourself worthy of the magical pizza recipe.\n")
+    print("As a reward, I present to you the secret recipe for the most magical pizza of all...\n")
+    print("The Pineapple Paradise Pizza!\n")
+    print("Thank you for playing the game. We hope you enjoyed your adventure!\n")
